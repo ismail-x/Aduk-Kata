a0d428a0720146a1d31889f676b332195c985de6
ismail <ismail.x@aol.com>
2020-09-18 02:16:54 +0700

    Initial Commit

---
+++ Aduk Kata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aduk Kata
+//
+//  Created by Ismail . on 18/09/20.
+//  Copyright © 2020 Ismail . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
